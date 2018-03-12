2ac72a85a56e5472cba1d010eb5397d95568552b
Botmatic <36082151+botmatic@users.noreply.github.com>
2018-03-12 15:09:03 +0100

    update the path to dotenv

--- src/index.js
+++ src/index.js
@@ -21,7 +21,9 @@
 
 //
 const debug = require('debug')('botmatic:js-contact')
-require('dotenv').config()
+require('dotenv').config({
+  path: path.join(__dirname, '/../.env')
+})
 
 const zip = rows => rows[0].map((_,c) => rows.map(row => row[c]))
 
